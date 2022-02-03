9080a594662e4fa88b3e8c21aba4519525268e4e
ConorEganLE <conor@leaneasy.co.uk>
2022-02-03 15:14:32 +0000

    init

---
+++ scheduled_actions/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Scheduled Actions Example',
+    'version': '1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'An example of how to create scheduled actions in Odoo',
+    'author': "Lean Easy",
+    'website': "https://www.leaneasy.co.uk",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/employee_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
