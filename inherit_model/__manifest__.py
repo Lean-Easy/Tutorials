c369b95e2b1a56d225add95e1251a26bb59db54b
Conor Egan <86407426+ConorEganLE@users.noreply.github.com>
2022-02-07 15:56:42 +0000

    init

---
+++ inherit_model/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Inherit Model Example',
+    'version': '1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'An example of how to inherit a model in Odoo.',
+    'author': "Lean Easy",
+    'website': "https://www.leaneasy.co.uk",
+    'depends': ['base'],
+    'data': [
+        'views/employee_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ inherit_model/models/employee.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields, models
+
+
+class Employee(models.Model):
+
