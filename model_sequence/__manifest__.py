c25857e96d4906289d3ebd2d8a45957c6cc279ba
Conor Egan <86407426+ConorEganLE@users.noreply.github.com>
2022-02-08 15:52:03 +0000

    model sequence

--- model_sequence/__manifest__.py
+++ model_sequence/__manifest__.py
@@ -10,6 +10,8 @@
     'data': [
         'security/ir.model.access.csv',
         'views/employee_view.xml',
+
+        'data/ir_sequence.xml'
     ],
     'installable': True,
     'auto_install': False,
--- model_sequence/models/employee.py
+++ model_sequence/models/employee.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class Employee(models.Model):
     _name = "employee.employee"
+
+    name = fields.Char(string="Employee Number", required=True, readonly=True, copy=False, default=_('New'))
 
     forename = fields.Char(string="Forename")
     surname = fields.Char(string="Surname")
@@ -17,3 +19,10 @@ class Employee(models.Model):
     salary = fields.Monetary(string="Salary", currency_field='currency_id')
     photo = fields.Image(string="Photo")
     nationality = fields.Many2one('res.country', string="Nationality")
+
+    @api.model
+    def create(self, vals_list):
+        if vals_list.get('name', _('New')) == _('New'):
+            vals_list['name'] = self.env['ir.sequence'].next_by_code('employee.employee') or _('New')
+
+        return super(Employee, self).create(vals_list)
