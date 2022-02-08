66d61684649fa4a937a7fa044307b8ff819c94cd
Conor Egan <86407426+ConorEganLE@users.noreply.github.com>
2022-02-08 13:14:58 +0000

    init

---
+++ model_sequence/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Model Sequence Example',
+    'version': '1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'An example of how to create a sequence for a model.',
+    'author': "Lean Easy",
+    'website': "https://www.leaneasy.co.uk",
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/employee_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
